389ada281ba0396c32e662b3957264f337b57e86
elmi-elmi <shahrokhelmi@gmail.com>
2021-11-14 23:29:52 +0330

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Field as='textarea' row='5' placeholder="Your comment here..."/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+import {Field} from 'vee-validate';
+const app =  createApp(App);
+app.component('Field', Field);
+app.mount('#app');
